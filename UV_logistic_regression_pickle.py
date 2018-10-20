8d5d0b00d2eb30cf47afae391761e2d270a7cc3f
echounlucky <k_munday@hotmail.com>
2018-10-21 08:15:11 +1300

    Update UV_logistic_regression_pickle.py

--- UV_logistic_regression_pickle.py
+++ UV_logistic_regression_pickle.py
@@ -1,14 +1,15 @@
-# For the assignment, delete the hash at the start of lines 3 and 4 to change the home directory for this file.
-# Change the directory name in line 4 to the folder where you have saved the [NBA] data for the assignment. 
+# For the assignment, delete the hash at the start of lines 4 and 5. 
+# Running the code in these lines tells pyton to change the home directory for this file.
+# Change the directory name in line 5 to the folder where you have saved the [NBA] data for the assignment. 
 # import os
-# os.chdir('C:\\Folder_name\\Sub_folder_name')
+# os.chdir('C:\\Folder_name\\Sub_folder_name') 
 
 import pandas
 from sklearn import linear_model
 from sklearn.externals import joblib
 
 data = "https://raw.githubusercontent.com/echounlucky/ML-for-SB/master/TennisData.csv" 
-# For the assignment, change the file name to the csv file you have saved in the directory above
+# For the assignment, change the file name to the csv file you have saved in the directory above, e.g. 'NBAData.csv'
 dataset = pandas.read_csv(data, header=None)
 
 array = dataset.values
