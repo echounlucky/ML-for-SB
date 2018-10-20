9ee34dec40d6028ebc532a9294afc6e7ab4eb589
echounlucky <k_munday@hotmail.com>
2018-10-21 08:32:17 +1300

    Update UV_logistic_regression_predict.py

--- UV_logistic_regression_predict.py
+++ UV_logistic_regression_predict.py
@@ -1,12 +1,14 @@
-import os
-os.chdir('C:\\Users\\k_mun\\Documents\\Tennis\\Machine learning') # Change directory to where you have saved your data
+# As with UV_logistic_regression, delete the hash at the start of lines 2 and 3 for the assignment. 
+# import os
+# os.chdir('C:\\Folder_name\\Sub_folder_name')
 
 import pandas
 from sklearn.externals import joblib
 
 lr = joblib.load('Predictor.pkl')
 
-data = 'UpcomingMatches.csv' # Save data on about 10 matches under this file name in the directory above
+data = 'https://raw.githubusercontent.com/echounlucky/ML-for-SB/master/TennisData.csv' 
+# For the assignment, replace the URL with the name of the file name that you saved in the folder above, e.g. 'NBAData.csv' 
 dataset = pandas.read_csv(data, header=None)
 
 probabilities = lr.predict_proba(dataset)
